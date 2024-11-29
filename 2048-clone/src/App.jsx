0f19c9064e0dbfb2c7f2cd1fcd0f9cb5d6d7732d
Zitian Qin <z5476244@ad.unsw.edu.au>
2024-11-30 03:33:14 +1100

    Fix tailwind installation

--- 2048-clone/src/App.jsx
+++ 2048-clone/src/App.jsx
@@ -2,10 +2,10 @@ import Game from "./game";
 
 function App() {
   return (
-    <>
+    <div className="h-screen bg-red-500">
       <h1>2048 Clone</h1>
       <Game />
-    </>
+    </div>
   );
 }
 
